3cf9b0ebb8dbd99553c429868b84f598533705fb
elosine <justinwenloyang@gmail.com>
2021-06-04 13:16:10 -0400

    u

--- soundflow_1b/public/usefulFunctions.js
+++ soundflow_1b/public/usefulFunctions.js
@@ -422,6 +422,22 @@ function probability(n) {
   return !!n && Math.random() <= n;
 };
 
+function mkSVGArc(x, y, radius, startAngle, endAngle){
+
+    var start = polarToCartesian(x, y, radius, endAngle);
+    var end = polarToCartesian(x, y, radius, startAngle);
+
+    var largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1"; //if then shorthand
+
+    var d = [
+        "M", start.x, start.y,
+        "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y
+    ].join(" ");
+
+    return d;
+}
+//to use: document.getElementById("arc1").setAttribute("d", describeArc(200, 400, 100, 0, 180))
+
 
 
 
